213c5c91df9b93ea182b7d836b76adce38ca6990
Joshualite <joshualite@hotmail.com>
2021-10-09 14:32:15 -0500

    Adding the election analysis.

--- Pypoll.py
+++ Pypoll.py
@@ -50,43 +50,67 @@ with open(file_to_load) as election_data:
          candidate_votes[candidate_name] += 1 
 
 
-# 3. Print the total votes.
-print(candidate_votes)
 
-# Determine the percentage of votes for each candidate by looping through the counts.
-# 1. Iterate through the candidate list.
-for c_name in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
-    votes = candidate_votes[c_name]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
+# Save the results to our text file.
+with open(file_to_save,"w") as txt_file:
+
+         # Print the final vote count to the terminal.
+         election_results = (
+             f"\nElection results\n"
+             f"------------------\n"
+             f"Total votes :{total_votes:,}\n"
+             f"------------------\n")
+
+         print(election_results, end = "")     
+         # Save the final vote count to the text file.
+         txt_file.write(election_results)
+
+
+         # Determine the percentage of votes for each candidate by looping through the counts.
+         # 1. Iterate through the candidate list.
+         for c_name in candidate_votes:
+          # 2. Retrieve vote count of a candidate.
+             votes = candidate_votes[c_name]
+              # 3. Calculate the percentage of votes.
+             vote_percentage = float(votes) / float(total_votes) * 100
     
-    # To do: print out each candidate's name, vote count, and percentage of
-    # votes to the terminal.
-    print(f"{c_name}: {vote_percentage:.1f}% ({votes:,})\n")
+             # To do: print out each candidate's name, vote count, and percentage of
+             # votes to the terminal.
+             # print(f"{c_name}: {vote_percentage:.1f}% ({votes:,})\n")
+             # Print each candidate, their voter count, and percentage to the terminal.
+             candidate_results = (f'{c_name}: {vote_percentage:.1f}% ({votes:,})\n')
+             print(candidate_results)
+             #  Save the candidate results to our text file.
+             txt_file.write(candidate_results)
 
 
-    # Determine winning vote count and candidate
-    # Determine if the votes is greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # If true then set winning_count = votes and winning_percent =
-        # vote_percentage.
-        winning_count = votes
-        winning_percentage = vote_percentage
-        # And, set the winning_candidate equal to the candidate's name.
-        winning_candidate = c_name
+             # Determine winning vote count and candidate
+             # Determine if the votes is greater than the winning count.
+             if (votes > winning_count) and (vote_percentage > winning_percentage):
+                  # If true then set winning_count = votes and winning_percent =
+                  # vote_percentage.
+                 winning_count = votes
+                 winning_percentage = vote_percentage
+                 # And, set the winning_candidate equal to the candidate's name.
+                 winning_candidate = c_name
 
 
 #  To do: print out the winning candidate, vote count and percentage to
 #   terminal.
 
-winning_candidate_summary = (
-    f'---------------------\n'
-    f'Winner: {winning_candidate}\n'
-    f'Winning vote count : {winning_count:,}\n'
-    f'Winning percentage : {winning_percentage:.1f}%\n'
-    f'---------------------\n')
+         winning_candidate_summary = (
+             f'---------------------\n'
+             f'Winner: {winning_candidate}\n'
+             f'Winning vote count : {winning_count:,}\n'
+             f'Winning percentage : {winning_percentage:.1f}%\n'
+             f'---------------------\n')
 
-print(winning_candidate_summary)
+         print(winning_candidate_summary)
+
+         txt_file.write(winning_candidate_summary)
+
+
+
+
 
 
